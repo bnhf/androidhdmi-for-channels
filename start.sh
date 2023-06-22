9d7e57f11125642149af741048ee2d99136e972e
Scott Ueland <41088895+bnhf@users.noreply.github.com>
2023-06-22 11:25:01 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /opt/scripts/
+
 adb start-server
 
 streamers=( $STREAMER1_IP $STREAMER2_IP $STREAMER3_IP $STREAMER4_IP )
@@ -13,17 +15,19 @@ for i in "${streamers[@]}"
 
 adb devices
 
+mkdir -p ./sample/yttv
+
 files=( prebmitune.sh bmitune.sh stopbmitune.sh )
 
 for i in "${files[@]}"
   do
     if [ ! -f /opt/scripts/sample/yttv/$i ]; then
-      cp /go/src/github.com/bnhf/sample/yttv/$i /opt/scripts/sample/yttv \
-      && chmod +x /opt/scripts/sample/yttv/$i \
+      cp /go/src/github.com/bnhf/sample/yttv/$i ./sample/yttv \
+      && chmod +x ./sample/yttv/$i \
       && echo "No existing $i found"
     else
       echo "Existing $i found, and will be used"
   fi
 done
 
-/opt/androidhdmi-for-channels$TUNERS
+../androidhdmi-for-channels$TUNERS
